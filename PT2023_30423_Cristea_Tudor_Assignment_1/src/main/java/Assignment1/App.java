51be01b85d3e2c9b258c1b098c992444a744aab1
sfr6 <cristeatudor6@gmail.com>
2023-03-24 14:18:41 +0200

    first commit

---
+++ PT2023_30423_Cristea_Tudor_Assignment_1/src/main/java/Assignment1/App.java
@@ -0,0 +1,14 @@
+package Assignment1;
+
+import Assignment1.controllers.MainController;
+import Assignment1.views.MainView;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        MainView mainView = new MainView();
+
+        MainController mainController = new MainController(mainView);
+    }
+}
